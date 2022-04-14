f9fa9138c78effd3fe2a199e0bb21b374879dac9
saurabh agarwal <53713306+saurabhdev21@users.noreply.github.com>
2022-04-14 23:46:28 +0530

    2022-04-14:- created table structure.

--- judge/home/models.py
+++ judge/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+# makemigrations - create changes and store in file
+# migrate - apply the pending changes created by makemigrations
+
+class Problem(models.Model):
+    pid = models.BigIntegerField()
+    email = models.CharField(max_length= 122)
+    phone = models.CharField(max_length= 13)
+    suggestions = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.pid
+
+class Submission(models.Model):
+    sid = models.BigIntegerField()
